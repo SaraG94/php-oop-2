02d7d086b0c0ce44f70fe32a3245c2b86962835b
SaraG94 <sara.gherbassi.94@gmail.com>
2023-04-02 21:10:55 +0200

    fix var_dump($cibo1);

--- Products.php
+++ Products.php
@@ -4,10 +4,11 @@ require_once "Food.php";
 require_once "Games.php";
 require_once "Kennels.php";
 
-$prodotto1 = new Product("marca di cibo", 20, "categoriaGatto","generico");
+
 
 $cibo1 = new Food("dfsf,fgdg,gd",22-10-2054);
-var_dump($prodotto1,$cibo1);
+$prodotto1 = new Product("marca di cibo", 20, "categoriaGatto", $cibo1);
+var_dump($cibo1);
 
 
 ?>
